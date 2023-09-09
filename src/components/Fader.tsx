c9a5ffe7036703c056f8936120bede73ffeb323f
bananu7 <bananu7@o2.pl>
2023-09-09 23:46:08 +0200

    Added scrolling to faders. Closes #30.

--- src/components/Fader.tsx
+++ src/components/Fader.tsx
@@ -12,11 +12,23 @@ export function Fader(props: FaderProps) {
         props.setValue(vol);
     }, [props.setValue]);
 
+    const wheelHandler = useCallback((e: WheelEvent<HTMLDivElement>) => {
+        const delta = e.deltaY / 100 * 2;
+
+        if ((e.deltaY < 0 && props.value < 100) ||
+            (e.deltaY > 0 && props.value > 0))
+        {
+            let newVal = props.value - delta;
+            newVal = newVal < 0 ? 0 : newVal;
+            newVal = newVal > 100 ? 100 : newVal;
+            props.setValue(newVal);
+        }
+    }, [props.value]);
 
     const className = "fader" + (props.color ? (" " + props.color) : "");
 
     return (
-        <div className={className}>
+        <div className={className} onWheel={wheelHandler}>
             <input type="range" value={props.value} onChange={onChange}></input>
         </div>
     );
