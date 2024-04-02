0ca91b84ebfaab9b21d53559da4934bc55b1bb38
bananu7 <bananu7@o2.pl>
2024-04-03 00:09:00 +0200

    Pad human-readable times with zeros

--- src/zoom/formatProjectTime.ts
+++ src/zoom/formatProjectTime.ts
@@ -1,9 +1,17 @@
 import { ProjectTimeSeconds } from './zoom_l12'
 
+function padWithZero(n) {
+    if (n < 10) {
+        return `0${n}`;
+    } else {
+        return `${n}`;
+    }
+}
+
 export function formatProjectTime(time: ProjectTimeSeconds) {
     const hours = Math.floor(time / 3600);
     time -= hours * 3600;
     const minutes = Math.floor(time / 60);
     time -= minutes * 60;
-    return `${hours}:${minutes}:${Math.floor(time)}`;
+    return `${padWithZero(hours)}:${padWithZero(minutes)}:${padWithZero(Math.floor(time))}`;
 }
